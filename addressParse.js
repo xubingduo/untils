8fc0fd0637e21cb2274d84395bc97535baa895ba
xbu <1243364439@qq.com>
2020-06-15 09:28:07 +0800

    '地址拆分'

--- addressParse.js
+++ addressParse.js
@@ -63,14 +63,21 @@ function addressParse(address) {
     .replace(/\n/g, ' ')
     .replace(/\t/g, ' ');
 
+  const arr = address.split(' ');
+  const newArr = [];
+  arr.forEach(el => {
+    newArr.push(el.replace(/(.*：)/g, ''));
+  });
+  address = newArr.join(' ');
   const search = [
     '地址',
     '收货地址',
     '收货人',
     '收件人',
     '收货',
-    '邮编',
     '电话',
+    '联系方式',
+    '联系人',
     '：',
     ':',
     '；',
@@ -79,6 +86,7 @@ function addressParse(address) {
     ',',
     '。'
   ];
+
   search.forEach(str => {
     address = address.replace(new RegExp(str, 'g'), ' ');
   });
@@ -114,47 +122,70 @@ function addressParse(address) {
   }
 
   address = address.replace(/ {2,}/, ' ');
+  const detail = detailParseForward(address.trim());
+  if (!detail.province) {
+    throw Error('请核对信息格式，地址无法匹配');
+  }
 
-  let detail = detailParseForward(address.trim());
-
+  // 如果没有城市匹配上 比如用户输入 孙军，17856785546，重庆市开州区行将路1021号迎春小区35憧504室
   if (!detail.city) {
-    detail = detailParse(address.trim());
-    if (detail.area && !detail.city) {
-      detail = detailParse(address.trim(), {
-        ignoreArea: true
-      });
-      // console.log('smart_parse->ignoreArea');
-    } else {
-      // console.log('smart_parse');
+    // 获取省市区数组
+    let objAddr = {};
+    for (let i = 0; i < defaultData.length; i++) {
+      if (defaultData[i].name === detail.province) {
+        objAddr = defaultData[i];
+        break;
+      }
     }
-    //这个待完善
-    const list = address
-      .replace(detail.province, '')
-      .replace(detail.city, '')
-      .replace(detail.area, '')
-      .split(' ')
-      .filter(str => str);
-    if (list.length > 1) {
-      list.forEach(str => {
-        if (!parse.name || (str && str.length < parse.name.length)) {
-          parse.name = str.trim();
+
+    let detailArr = detail.addr;
+    let areaArr = [];
+
+    // 处理多个直辖市
+    objAddr.city.forEach(el => {
+      if (detailArr.indexOf(el.name) > -1 && detailArr.indexOf(el.name) < 6) {
+        detailArr = detailArr.substr(detailArr.name.length);
+        areaArr = el.area;
+        detail.city = el.name;
+      }
+    });
+
+    if (!detail.city) {
+      detail.city = detail.province;
+      objAddr.city.forEach(el => {
+        if (el.name === detail.city) {
+          areaArr = el.area;
+          if (
+            detailArr.indexOf(el.name) > -1 &&
+            detailArr.indexOf(el.name) < 5
+          ) {
+            detailArr = detailArr.substr(el.name.length);
+          }
         }
       });
-      if (parse.name) {
-        detail.addr = detail.addr.replace(parse.name, '').trim();
+    }
+
+    for (let i = 0; i < areaArr.length; i++) {
+      if (
+        detailArr.indexOf(areaArr[i]) > -1 &&
+        detailArr.indexOf(areaArr[i]) < 5
+      ) {
+        detail.area = areaArr[i];
+        detailArr = detailArr.substr(areaArr[i].length);
+        detail.addr = detailArr.trim();
       }
     }
+  }
+
+  if (detail.name) {
+    parse.name = detail.name;
   } else {
-    if (detail.name) {
-      parse.name = detail.name;
-    } else {
-      const list = detail.addr.split(' ').filter(str => str);
-      if (list.length > 1) {
-        parse.name = list[list.length - 1];
-      }
-      if (parse.name) {
-        detail.addr = detail.addr.replace(parse.name, '').trim();
-      }
+    const list = detail.addr.split(' ').filter(str => str);
+    if (list.length > 1) {
+      parse.name = list[list.length - 1];
+    }
+    if (parse.name) {
+      detail.addr = detail.addr.replace(parse.name, '').trim();
     }
   }
 
@@ -174,7 +205,7 @@ function addressParse(address) {
  * @param address
  * @returns {{province: string, city: string, area: string, addr: string}}
  */
-function detailParseForward(addr) {
+function detailParseForward(address) {
   const parse = {
     province: '',
     city: '',
@@ -182,20 +213,6 @@ function detailParseForward(addr) {
     addr: '',
     name: ''
   };
-
-  const getAddr = addr.split(' ');
-  let address = getAddr[0];
-
-  for (let i = 0; i < getAddr.length; i++) {
-    if (address) {
-      if (address.length <= getAddr[i].length) {
-        address = getAddr[i];
-      } else {
-        parse.name = getAddr[i];
-      }
-    }
-  }
-
   const provinceKey = [
     '特别行政区',
     '古自治区',
@@ -225,65 +242,48 @@ function detailParseForward(addr) {
   ];
 
   for (const i in defaultData) {
+    // 省名称
     const province = defaultData[i];
     let index = address.indexOf(province.name);
+    // 判断省名称是否在前五位
     if (index > -1 && index < 6) {
+      if (index > 0) {
+        //省份不是在第一位，在省份之前的字段识别为名称
+        parse.name = address.substr(0, index).trim();
+      }
       parse.province = province.name;
+      // 截取除省之外的字符串
       address = address.substr(index + province.name.length);
       for (const k in provinceKey) {
         if (address.indexOf(provinceKey[k]) === 0) {
           address = address.substr(provinceKey[k].length);
         }
       }
-
-      // 如果是直辖市 比如 北京 上海 天津
-      if (province.city.length === 1) {
-        parse.city = province.city[0].name;
-        for (const k in cityKey) {
-          if (address.indexOf(cityKey[k]) === 0) {
-            address = address.substr(cityKey[k].length);
-          }
-        }
-        const city = province.city[0];
-        if (city.area) {
-          for (const k in city.area) {
-            const area = city.area[k];
-            index = address.indexOf(area);
-            if (index > -1 && index < 3) {
-              parse.area = area;
-              address = address.substr(index + parse.area.length);
-              break;
+      for (const j in province.city) {
+        const city = province.city[j];
+        index = address.indexOf(city.name);
+        if (index > -1 && index < 3) {
+          parse.city = city.name;
+          address = address.substr(index + parse.city.length);
+          for (const k in cityKey) {
+            if (address.indexOf(cityKey[k]) === 0) {
+              address = address.substr(cityKey[k].length);
             }
           }
-        }
-      } else {
-        for (const j in province.city) {
-          const city = province.city[j];
-          index = address.indexOf(city.name);
-          if (index > -1 && index < 3) {
-            parse.city = city.name;
-            address = address.substr(index + parse.city.length);
-            for (const k in cityKey) {
-              if (address.indexOf(cityKey[k]) === 0) {
-                address = address.substr(cityKey[k].length);
+          if (city.area) {
+            for (const k in city.area) {
+              const area = city.area[k];
+              index = address.indexOf(area);
+              if (index > -1 && index < 3) {
+                parse.area = area;
+                address = address.substr(index + parse.area.length);
+                break;
               }
             }
-            if (city.area) {
-              for (const k in city.area) {
-                const area = city.area[k];
-                index = address.indexOf(area);
-                if (index > -1 && index < 3) {
-                  parse.area = area;
-                  address = address.substr(index + parse.area.length);
-                  break;
-                }
-              }
-            }
-            break;
           }
+          break;
         }
       }
-
       parse.addr = address.trim();
       break;
     }
@@ -298,7 +298,7 @@ function detailParseForward(addr) {
  * @param ignoreArea 是否忽视区 因为地址中含有区容易导致匹配错误 例：山东省蓬莱市黄海花园东区西门宝威学堂 曲荣声收15753572456
  * @returns {{province: string, city: string, area: string, name: string, areaCopy: string, addr: string}}
  */
-function detailParse(address, {ignoreArea = false} = {}) {
+export function detailParse(address, {ignoreArea = false} = {}) {
   const parse = {
     province: '',
     city: '',
